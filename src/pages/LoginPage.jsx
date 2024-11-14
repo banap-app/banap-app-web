652a84bb9599f9d42e2db39f061054cb8db891f3
Victor Luís <victorluiscaguiar@gmail.com>
2024-11-14 20:52:40 -0300

    feat: minor changes on login screen

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,28 +5,33 @@ const LoginPage = () => {
         <div className="flex h-screen w-screen">
             <div className="flex h-full w-full flex-col items-center justify-center gap-6">
                 <form>
-                    <div className="flex flex-col gap-10">
-                        <div className="flex flex-col gap-5">
-                            <div className="relative">
-                                <input
-                                    type="email"
-                                    placeholder="E-mail"
-                                    className="h-20 w-full max-w-md rounded-lg border-2 border-[#D0D0D0] px-20 text-2xl text-[#8F8F8F] focus:outline-none"
-                                />
-                                <div className="absolute left-6 top-6">
-                                    <Mail color="#8F8F8F" size={"32px"} />
+                    <div className="flex flex-col gap-8">
+                        <div className="flex flex-col gap-3">
+                            <div className="flex flex-col gap-5">
+                                <div className="relative">
+                                    <input
+                                        type="email"
+                                        placeholder="E-mail"
+                                        className="h-20 w-full max-w-md rounded-lg border-2 border-[#D0D0D0] px-20 text-2xl text-[#8F8F8F] focus:outline-none"
+                                    />
+                                    <div className="absolute left-6 top-6">
+                                        <Mail color="#8F8F8F" size={"32px"} />
+                                    </div>
+                                </div>
+                                <div className="relative">
+                                    <input
+                                        type="password"
+                                        placeholder="Senha"
+                                        className="h-20 w-full max-w-md rounded-lg border-2 border-[#D0D0D0] px-20 text-2xl text-[#8F8F8F] focus:outline-none"
+                                    />
+                                    <div className="absolute left-6 top-6">
+                                        <Lock color="#8F8F8F" size={"32px"} />
+                                    </div>
                                 </div>
                             </div>
-                            <div className="relative">
-                                <input
-                                    type="password"
-                                    placeholder="Senha"
-                                    className="h-20 w-full max-w-md rounded-lg border-2 border-[#D0D0D0] px-20 text-2xl text-[#8F8F8F] focus:outline-none"
-                                />
-                                <div className="absolute left-6 top-6">
-                                    <Lock color="#8F8F8F" size={"32px"} />
-                                </div>
-                            </div>
+                            <p className="text-right text-xl text-banapLight">
+                                Esqueceu sua senha?
+                            </p>
                         </div>
                         <input
                             type="submit"
@@ -35,6 +40,12 @@ const LoginPage = () => {
                         />
                     </div>
                 </form>
+                <div className="relative w-[258px]">
+                    <hr className="w-full border-t border-[#8F8F8F]" />
+                    <p className="absolute left-[114.6px] top-[-18px] bg-white p-1 text-xl text-[#8F8F8F]">
+                        ou
+                    </p>
+                </div>
                 <p className="text-center text-xl text-[#8F8F8F]">
                     Não possui uma conta?{" "}
                     <a className="text-banapLight">Cadastre-se</a>
